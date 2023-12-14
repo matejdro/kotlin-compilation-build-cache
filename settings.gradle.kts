e9f57aae3db454deb7cc160e3a177c02ff1baa82
Matej Drobnič <matej@matejdro.com>
2023-12-14 12:21:20 +0100

    attempt to use remote build cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
 rootProject.name = "sample"
 includeBuild("build-conventions")
 include("application", "utilities", "list", "dummy-project-a", "dummy-project-b", "dummy-project-c", "dummy-project-d", "dummy-project-e", "dummy-project-f")
+
+buildCache {
+    local {
+        isEnabled = false
+    }
+    remote<HttpBuildCache> {
+        isEnabled = true
+        isPush = true
+        isAllowUntrustedServer = true
+        setUrl("http://localhost:5071/cache")
+    }
+}
